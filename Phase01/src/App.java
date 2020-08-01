74cc52f3024a6c1f70e6b6116b794cef34254f4d
Parsa <parsa2820@gmail.com>
2020-08-01 16:44:00 +0430

    Implement InvertedIndex class and command line view

--- Phase01/src/App.java
+++ Phase01/src/App.java
@@ -1,5 +1,25 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        FileReader fileReader = new FileReader();
+        InvertedIndex invertedIndex = new InvertedIndex();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Search for a single word : ");
+        String word = scanner.nextLine();
+        try {
+            HashSet<String> result = invertedIndex.findSingleWord(word);
+            result.forEach(new Consumer<String>(){
+                @Override
+                public void accept(String t) {
+                    System.out.println(t);
+                }
+            });
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } 
+        scanner.close();
     }
 }
--- Phase01/src/InvertedIndex.java
+++ Phase01/src/InvertedIndex.java
@@ -1,3 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class InvertedIndex {
-    
+
+    HashMap<String, HashSet<String>> dictionary;
+
+    public InvertedIndex() {
+        dictionary = new HashMap<>();
+    }
+
+    public void add(String word, String documentName) {
+        if (dictionary.containsKey(word)) {
+            dictionary.get(word).add(documentName);
+            return;
+        }
+        HashSet<String> documentsName = new HashSet<>();
+        documentsName.add(documentName);
+        dictionary.put(word, documentsName);
+    }
+
+    public HashSet<String> findSingleWord(String word) throws Exception {
+        if (dictionary.containsKey(word)) {
+            return dictionary.get(word);
+        }
+        throw new Exception("No items match your search");
+    }
 }
