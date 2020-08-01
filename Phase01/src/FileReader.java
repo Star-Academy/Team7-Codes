f3a4c98921dc2fe979ce58cc13bbeeeeb93b343c
hassan <h.zaker2222@gmail.com>
2020-08-01 17:22:54 +0430

    FileReader

--- Phase01/src/FileReader.java
+++ Phase01/src/FileReader.java
@@ -1,3 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+
 public class FileReader {
-    
+    InvertedIndex invertedIndex = new InvertedIndex();
+    public FileReader() {
+
+    }
+
+    public void readAllFiles(){
+        File path = new File("/home/hassan/code-star/Team7-Codes/Phase01/src/documents/");
+        File[] documents = path.listFiles();
+        readFile(documents[0]);
+    }
+
+    public void readFile(File document) {
+        try {
+            Scanner input = new Scanner(document);
+            // System.out.println(document.getName());
+            while (input.hasNext()) {
+                String line = input.next();
+                invertedIndex.add(line, document.getName());
+                // System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
