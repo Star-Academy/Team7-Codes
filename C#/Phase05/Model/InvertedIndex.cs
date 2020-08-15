1298b42460bf07e27be7cfbbffbcfacaff1c6ca7
hassan <h.zaker2222@gmail.com>
2020-08-15 12:44:02 +0430

    tests added for Add method in InvertedIndex

--- C#/Phase05/Model/InvertedIndex.cs
+++ C#/Phase05/Model/InvertedIndex.cs
@@ -5,6 +5,13 @@ namespace Phase05.Model
 {
     public class InvertedIndex<T, E> : IIndex<T, E>
     {
+        private Dictionary<IToken<T>, HashSet<ITokenInfo<E>>> Dictionary ;
+
+        public InvertedIndex(Dictionary<IToken<T>, HashSet<ITokenInfo<E>>> dictionary)
+        {
+            this.Dictionary = dictionary;
+        }
+
         public void Add(IAddQuery<T, E> addQuery)
         {
             throw new System.NotImplementedException();
