300de6b6260011af029a75f020c0ab40612b20e1
hassan <h.zaker2222@gmail.com>
2020-08-23 11:25:02 +0430

    ElasticFinderConsoleApp Created

---
+++ C#/Phase08/ElasticFinderConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Nest;
+
+
+namespace ElasticFinderConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        
+        }
+    }
+}
