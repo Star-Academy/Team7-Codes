ec6fb7c7388f2fda3bf7ab729157f5e86182a2d6
Parsa <parsa2820@gmail.com>
2020-09-06 09:51:10 +0430

    Finish front-end

--- Front-end/Phase09/script.js
+++ Front-end/Phase09/script.js
@@ -1,5 +1,9 @@
+var includeWordsList = [];
+
 function init() {
     const query = queryStringFormatter(window.location.search);
+    extractWordsList(query);
+    document.getElementById('search-word').innerHTML = query;
     const results = getResult(query);
 }
 
@@ -24,5 +28,32 @@ function getResult(query) {
 
 
 function showResult(results) {
-    console.log(results);
+    const resultElement = document.getElementById('result');
+    for (const item of results) {
+        const content = highlightWords(item.content);
+        resultElement.innerHTML += `<details class="card single-result">
+        <summary>${item.name}</summary>
+        ${content}
+        </details>`;
+    }
+}
+
+function extractWordsList(query = 'a') {
+    let words = query.split(' ');
+    for (const word of words) {
+        if (word.startsWith('+')) {
+            includeWordsList.push(word.substring(1));
+        } else if (!word.startsWith('-')){
+            includeWordsList.push(word);
+        }
+    }
+}
+
+function highlightWords(content) {
+    let newContent = content;
+    for (const word of includeWordsList) {
+        let regex = new RegExp(word, 'ig');
+        newContent = newContent.replaceAll(regex, `<span class="highlighted">${word}</span>`);
+    }
+    return newContent;
 }
